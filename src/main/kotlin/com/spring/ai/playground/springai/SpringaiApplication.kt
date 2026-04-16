cfa831d26aa103361f8ef5d23ae7c80c4fc61fd9
sujeong.lee <developercosj@gmail.com>
2026-04-16 10:28:54 +0900

    Feat: spring ai 공부 초기 설정

---
+++ src/main/kotlin/com/spring/ai/playground/springai/SpringaiApplication.kt
@@ -0,0 +1,11 @@
+package com.spring.ai.playground.springai
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringaiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringaiApplication>(*args)
+}
